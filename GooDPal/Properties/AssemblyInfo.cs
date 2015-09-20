dc8e438f211d93e73a87e3a7eb6913f1c2476772
TheCrafter <dimitrisvlh@gmail.com>
2015-09-21 00:19:48 +0300

    Added copyright notice to assembly info

--- GooDPal/Properties/AssemblyInfo.cs
+++ GooDPal/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GooDPal")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015 Vlachakis Dimitris")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
