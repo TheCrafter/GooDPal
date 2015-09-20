c4be4493cf30566161b282a4fd1c80519b9d1d50
TheCrafter <dimitrisvlh@gmail.com>
2015-09-21 00:26:51 +0300

    Changed project version in Asembly Info

--- GooDPal/Properties/AssemblyInfo.cs
+++ GooDPal/Properties/AssemblyInfo.cs
@@ -1,12 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GooDPal")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Google drive manager tool by TheCrafter.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GooDPal")]
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
