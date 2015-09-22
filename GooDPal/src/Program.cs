ac37948f694e5b8f19f9a24dcb6150f9a9a84b20
TheCrafter <dimitrisvlh@gmail.com>
2015-09-23 01:00:01 +0300

    Added a way to choose the remote root directory for sync-ing

--- GooDPal/src/Program.cs
+++ GooDPal/src/Program.cs
@@ -19,6 +19,9 @@ namespace GooDPal
 {
     class Program
     {
+        static string LocalDirPath = "C:\\Users\\TheCrafter\\Desktop\\Test";
+        static string RemoteDirPath = "";
+
         static void Main(string[] args)
         {
             DriveManager dMgr = new DriveManager();
@@ -40,24 +43,69 @@ namespace GooDPal
                 System.Environment.Exit(0);
 
             // Create directory structure from path
-            Directory dir = new Directory("C:\\Users\\TheCrafter\\Desktop\\Test");
+            Directory dir = new Directory(LocalDirPath);
             dir.Init();
+
+            // Find root sync folder id
+            string syncRootId = FindRemoteFolderIdByPath(dMgr, "root", ParseSyncRootString(RemoteDirPath));
+            
+            if(syncRootId == null)
+            {
+                Console.WriteLine("The path you requested is invalid.");
+                Environment.Exit(1);
+            }
 
             try
             {
                 Task.Run(async () =>
                 {
                     Drive.DirectorySynchronizer ds = new DirectorySynchronizer(dMgr);
-                    await ds.SyncDirectory(dir, "root");
+                    await ds.SyncDirectory(dir, syncRootId);
                 }).Wait();
             }
             catch (Exception e)
             {
-                Console.WriteLine("An error occured: " + e.Message);
+                Console.WriteLine("An error occured while synchronization process:\n" + e.Message);
             }
 
             Console.WriteLine("Press enter to continue . . .");
             Console.Read();
+        }
+
+        static List<string> ParseSyncRootString(string str)
+        {
+            string[] separators = new string[] { "/" };
+            return new List<string>(str.Split(separators, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        static string FindRemoteFolderIdByPath(DriveManager mgr, string id, List<string> foldersList)
+        {
+            IList<DriveFile> dirs = mgr.FetchChildrenDirectories(id);
+
+            // If at this point the list is empty it only means one thing.
+            // We're trying to find an empty path (""). The empty path
+            // is a special case and it means the root directory. So
+            // just return the root directory id.
+            if (foldersList.Count == 0)
+                return "root";
+
+            string curDir = foldersList[0];
+            foldersList.RemoveAt(0);
+
+            foreach (DriveFile dir in dirs)
+            {
+                if (dir.Title.Equals(curDir))
+                {
+                    // We dont have any more directories to search. This is it!!
+                    if (foldersList.Count == 0)
+                        return dir.Id;
+                    else
+                        // Continue the search to the next directory
+                        return FindRemoteFolderIdByPath(mgr, dir.Id, foldersList);
+                }
+            }
+
+            return null;
         }
     }
 }
